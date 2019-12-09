b0bba09072f117dd95aaa727f160e73f68baecdd
Edwin Hauspie <edwinhauspie@hotmail.com>
2019-12-09 16:50:20 +0100

    POC

--- static/app1.js
+++ static/app1.js
@@ -1,7 +1,15 @@
+/*
+TODO
+
+[ON HOLD] printen to image/pdf
+veldjes invullen / node selecteren en description invullen
+betere icons / hover state
+*/
+
 window.onload = () => {
     let M = mxClient;
 
-    //Browser checks
+    //Browser check
     if (!M || !M.isBrowserSupported) {
         alert('mxGraph not found.');
         return;
@@ -12,27 +20,27 @@ window.onload = () => {
         return;
     }
 
-    //Graph and editor setup
-    var container = document.getElementById('drawing');
+    //Setup and styling of graph and editor
+    var container = q('#drawing');
     var config = mxUtils.load('static/mxClient/resources/keyhandler-commons.xml').getDocumentElement();
     var editor = new mxEditor(config);
-    editor.setGraphContainer(container);
-    window.graph = editor.graph;
-    graph.setHtmlLabels(true);
-    graph.setPanning(true);
-    graph.setConnectable(true);
-    graph.setAllowDanglingEdges(false);
-    graph.setEdgeLabelsMovable(false);
-    graph.setTooltips(false);
-    graph.setCellsCloneable(false); //Prevent cloning cells by dragging them while holding down <ctrl>
-    mxEvent.disableContextMenu(container);
-    mxGraphHandler.prototype.guidesEnabled = true; // Enables guides
-    new mxRubberband(graph); //Enable multi mouse selection
-    mxEdgeHandler.prototype.addEnabled = true;
-    mxEdgeHandler.prototype.removeEnabled = true;
 
-    //Default styling
     (() => {
+        editor.setGraphContainer(container);
+        window.graph = editor.graph;
+        graph.setHtmlLabels(true);
+        graph.setPanning(true);
+        graph.setConnectable(true);
+        graph.setAllowDanglingEdges(false);
+        graph.setEdgeLabelsMovable(false);
+        graph.setTooltips(false);
+        graph.setCellsCloneable(false); //Prevent cloning cells by dragging them while holding down <ctrl>
+        mxEvent.disableContextMenu(container);
+        mxGraphHandler.prototype.guidesEnabled = true; // Enables guides
+        new mxRubberband(graph); //Enable multi mouse selection
+        mxEdgeHandler.prototype.addEnabled = true;
+        mxEdgeHandler.prototype.removeEnabled = true;
+
         //Set default edge style
         var style = {};
         style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_CONNECTOR;
@@ -65,161 +73,132 @@ window.onload = () => {
         style[mxConstants.STYLE_FILLCOLOR] = '#ffffff';
         style[mxConstants.STYLE_SPACING] = 15;
         style[mxConstants.STYLE_OVERFLOW] = 'hidden';
+        style[mxConstants.STYLE_FONTCOLOR] = '#000000';
         graph.getStylesheet().putDefaultVertexStyle(style);
     })();
 
-    /*graph.convertValueToString = function (cell) {
-        if (mxUtils.isNode(cell.value)) {
-            if (cell.value.nodeName.toLowerCase() == 'person') {
-                var firstName = cell.getAttribute('firstName', '');
-                var lastName = cell.getAttribute('lastName', '');
+    //Convert xml to json
+    function q(selector, ctx) {
+        let results = [];
 
-                if (lastName != null && lastName.length > 0) {
-                    return lastName + ', ' + firstName;
-                }
-
-                return firstName;
-            }
-            else if (cell.value.nodeName.toLowerCase() == 'knows') {
-                return cell.value.nodeName + ' (Since '
-                    + cell.getAttribute('since', '') + ')';
-            }
-
+        if (selector.nodeType) results.push(selector);
+        else if (selector instanceof Array) results = selector;
+        else {
+            if (!ctx) ctx = window.document;
+            else if (typeof ctx === 'string') ctx = window.document.querySelector(ctx);
+            results = [].slice.call(ctx.querySelectorAll(selector));
         }
 
-        return '';
-    };*/
-
-    /*try {
-        model.beginUpdate();
-        var parent = graph.getDefaultParent();
-        var defaultSize = { w: 140, h: 80 };
-        var v1 = graph.insertVertex(parent, null, 'Gebeurt de doorzoeking op heterdaad?', 40, 40, defaultSize.w, defaultSize.h);
-        var v2 = graph.insertVertex(parent, null, 'Geeft de eigenaar toestemming?', 320, 200, defaultSize.w, defaultSize.h);
-        var v3 = graph.insertVertex(parent, null, 'Je moet niks doen.', 40, 360, defaultSize.w, defaultSize.h);
-        addOverlays(graph, v1);
-        addOverlays(graph, v2);
-        addOverlays(graph, v3);
-        graph.insertEdge(parent, null, 'Ja', v1, v2);
-        graph.insertEdge(parent, null, 'Neen', v1, v3);
+        return results.length === 1 ? results[0] : results;
     }
-    finally {
-        model.endUpdate();
-    }*/
 
-    document.getElementById('fromXml').onclick = function () {
-        var doc = mxUtils.parseXml(document.getElementById('xml').value.trim());
+    function xmlToJson() {
+        var doc = mxUtils.parseXml(q('#xml').value.trim());
         var cells = [...doc.documentElement.getElementsByTagName('mxCell')];
         var nodeCells = cells.filter(c => c.getAttribute('vertex'));
         var edgeCells = cells.filter(c => c.getAttribute('edge'));
-        var attributesToObject = function (el) {
+
+        function attributesToObject(el) {
             return el ? [...el.attributes]
                 .map(y => ({ k: y.name, v: y.value }))
                 .reduce((agg, z) => { agg[z.k] = z.v; return agg; }, {})
                 : null;
-        };
+        }
 
         var nodes = nodeCells
             .map(n => {
                 var nn = attributesToObject(n);
                 return {
-                    id: nn.id.replace(/^_/, ''),
-                    start: nn.id == '_start' ? true : null,
-                    end: nn.id == '_end' ? true : null,
+                    id: nn.id, //.replace(/^_/, ''),
                     title: nn.value || null,
                     geometry: n.getElementsByTagName('mxGeometry')[0].outerHTML.replace(/\n\s*/g, ''),
+                    style: nn.style || null,
                     edges: edgeCells
                         .filter(e => e.getAttribute('source') == n.id)
                         .map(e => {
                             var ee = attributesToObject(e);
                             return {
-                                id: ee.id.replace(/^_/, ''),
+                                id: ee.id, //.replace(/^_/, ''),
                                 title: ee.value || null,
-                                target: ee.target.replace(/^_/, ''),
+                                target: ee.target, //.replace(/^_/, ''),
                                 geometry: e.getElementsByTagName('mxGeometry')[0].outerHTML.replace(/\n\s*/g, '')
                             };
                         })
                 };
             });
 
-        var obj = JSON.parse(document.getElementById('json').value);
-                if (obj.nodes) obj.nodes = nodes;
-        else obj = nodes;
-        document.getElementById('json').value = JSON.stringify(obj, (key, value) => { if (value !== null) return value }, '    ');
-    };
+        var obj = JSON.parse(q('#json').value || '{ "nodes": [] }');
+        obj.title = q('textarea', '#procTitle').value;
+        obj.contents = q('[contentEditable]', '#procDesc').innerHTML;
+        obj.nodes = nodes;
+        q('#json').value = JSON.stringify(obj, (key, value) => { if (value !== null) return value }, '    ');
+    }
 
+    q('#xmlToJson').onclick = xmlToJson;
+
+    //Convert json to xml
     function randomId() {
         var c = 'abcdefghijklmnopqrstuvwxyz'.split('');
         return c.map(x => c[Math.floor(Math.random() * 26)]).join('');
     }
 
-    document.getElementById('toXml').onclick = function () {
-        var obj = JSON.parse(document.getElementById('json').value);
+    /*var nodeGeometry = '<mxGeometry x="40" y="40" width="140" height="80" as="geometry"/>';
+    var startGeometry = '<mxGeometry x="40" y="40" width="50" height="50" as="geometry"/>';
+    var endGeometry = '<mxGeometry x="40" y="40" width="50" height="50" as="geometry"/>';
+    var edgeGeometry = '<mxGeometry relative="1" as="geometry"/>';*/
+    var startStyle = 'editable=0;resizable=0;shape=ellipse;perimeter=ellipsePerimeter;strokeWidth=2;strokeColor=#66CC00;';
+    var endStyle = 'editable=0;resizable=0;shape=ellipse;perimeter=ellipsePerimeter;strokeWidth=2;strokeColor=#FF6666;';
+
+    function jsonToXml() {
+        var obj = JSON.parse(q('#json').value);
         var xml = '';
 
-        var nodeGeometry = '<mxGeometry x="40" y="40" width="140" height="80" as="geometry"/>';
-        var startGeometry = '<mxGeometry x="40" y="40" width="50" height="50" as="geometry"/>';
-        var endGeometry = '<mxGeometry x="40" y="40" width="50" height="50" as="geometry"/>';
-        var nodeStyle = '';
-        var startStyle = 'editable=0;resizable=0;shape=ellipse;perimeter=ellipsePerimeter;strokeWidth=2;strokeColor=#66CC00;';
-        var endStyle = 'editable=0;resizable=0;shape=ellipse;perimeter=ellipsePerimeter;strokeWidth=2;strokeColor=#FF6666;';
-
-        function getNodeStyle(n) {
-            if (n.start) return startStyle;
-            if (n.end) return endStyle;
-            return nodeStyle;
-        }
-
-        function getNodeGeometry(n) {
-            if (n.geometry) return n.geometry;
-            if (n.start) return startGeometry;
-            if (n.end) return endGeometry;
-            return nodeGeometry;
+        function restoreLineBreaks(val) {
+            return val.replace('\n', '&#xa;');
         }
 
         (obj.nodes || obj).forEach(n => {
-            xml += `<mxCell id="_${n.id}" value="${n.title || ''}" style="${getNodeStyle(n)}" parent="1" vertex="1">
-                ${getNodeGeometry(n)}
-            </mxCell>`;
-
-            (n.edges || []).forEach(e => {
-                xml += `<mxCell id="_${e.id || randomId()}" value="${e.title || ''}" parent="1" source="_${n.id}" target="_${e.target}" edge="1">
-                    ${e.geometry || '<mxGeometry relative="1" as="geometry"/>'}
-                </mxCell>`;
-            });
+            xml += `<mxCell id="${n.id}" value="${restoreLineBreaks(n.title || '')}" style="${n.style || ''}" parent="1" vertex="1">${n.geometry}</mxCell>`;
+            (n.edges || []).forEach(e => (xml += `<mxCell id="${e.id || randomId()}" value="${restoreLineBreaks(e.title || '')}" parent="1" source="${n.id}" target="${e.target}" edge="1">${e.geometry}</mxCell>`));
         });
 
         var doc = mxUtils.parseXml(`<mxGraphModel><root><mxCell id="0"/><mxCell id="1" parent="0"/>${xml}</root></mxGraphModel>`);
-        document.getElementById('xml').value = mxUtils.getPrettyXml(doc.documentElement, '    ', '');
-    };
+        q('#xml').value = mxUtils.getPrettyXml(doc.documentElement, '    ', '');
+        q('textarea', '#procTitle').value = obj.title;
+        q('[contentEditable]', '#procDesc').innerHTML = obj.contents;
+    }
+
+    q('#jsonToXml').onclick = jsonToXml;
 
     //Auto arrange
-    document.getElementById('vertical').onclick = function () {
+    q('#vertical').onclick = function () {
         var layout = new mxHierarchicalLayout(graph);
         layout.execute(graph.getDefaultParent());
     };
 
-    document.getElementById('horizontal').onclick = function () {
+    q('#horizontal').onclick = function () {
         var layout = new mxHierarchicalLayout(graph, mxConstants.DIRECTION_WEST);
         layout.execute(graph.getDefaultParent());
     };
 
-    document.getElementById('organic').onclick = function () {
+    q('#organic').onclick = function () {
         var layout = new mxFastOrganicLayout(graph);
         layout.forceConstant = 120;
         layout.execute(graph.getDefaultParent());
     };
 
-    //Convert xml to grah
-    document.getElementById('fromGraph').onclick = function () {
+    //Convert graph to xml
+    function graphToXml() {
         var encoder = new mxCodec();
         var node = encoder.encode(graph.getModel());
-        document.getElementById('xml').value = mxUtils.getPrettyXml(node, '    ', '');
-    };
+        q('#xml').value = mxUtils.getPrettyXml(node, '    ', '');
+    }
 
-    //Convert graph to xml
-    document.getElementById('toGraph').onclick = function () {
-        var doc = mxUtils.parseXml(document.getElementById('xml').value);
+    q('#graphToXml').onclick = graphToXml;
+
+    //Convert xml to graph
+    function xmlToGraph() {
+        var doc = mxUtils.parseXml(q('#xml').value);
         var dec = new mxCodec(doc);
         dec.decode(doc.documentElement, editor.graph.getModel());
 
@@ -228,19 +207,21 @@ window.onload = () => {
             //if (v.id == '_start') v.setConnectable(false);
             //if (v.id == '_end') v.setConnectable(false);
         });
-    };
+    }
 
-    document.getElementById('load').onclick = async function () {
+    q('#xmlToGraph').onclick = xmlToGraph;
+
+    //Load and save json
+    q('#loadJson').onclick = async function () {
         var proc = await fetch(`static/procedureplayer/procedure3.json`).then(r => r.json());
-        document.getElementById('json').value = JSON.stringify(proc, null, '    ');
-        document.getElementById('toXml').click();
-        document.getElementById('toGraph').click();
-        //document.getElementById('vertical').click();
+        q('#json').value = JSON.stringify(proc, null, '    ');
+        jsonToXml();
+        xmlToGraph();
     };
 
-    document.getElementById('save').onclick = async function () {
+    q('#saveJson').onclick = async function () {
         var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(document.getElementById('json').value));
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(q('#json').value));
         element.setAttribute('download', 'procedure1.json');
         element.style.display = 'none';
         document.body.appendChild(element);
@@ -248,23 +229,21 @@ window.onload = () => {
         document.body.removeChild(element);
     };
 
-    /*document.getElementById('vertex').onclick = async function () {
+    /*$('#vertex').onclick = async function () {
         var v = graph.insertVertex(graph.getDefaultParent(), null, 'Nieuwe stap', 10, 10, 140, 80);
         addOverlays(graph, v);
     };*/
 
-    document.querySelectorAll('.enlarge').forEach(x => x.onclick = function (e) {
-        if (e.target.closest('.luik').style.flex.includes('.6')) {
-            document.querySelectorAll('.luik').forEach(x => x.style.flex = .3333)
-        }
-        else {
-            document.querySelectorAll('.luik').forEach(x => x.style.flex = .2)
-            e.target.closest('.luik').style.flex = .6;
+    //Panel enlarge
+    q('.enlarge').forEach(x => x.onclick = function (e) {
+        if (e.target.closest('.luik').style.flex.startsWith('1')) {
+            q('.luik').forEach(x => x.style.flex = 1)
+            e.target.closest('.luik').style.flex = 3;
         }
     });
 
     //Drag a new node
-    var afterDrag = function (graph, evt, target, x, y) {
+    var afterDrag1 = function (graph, evt, target, x, y) {
         var cell = new mxCell('Nieuwe stap', new mxGeometry(0, 0, 140, 80));
         cell.vertex = true;
         var cells = graph.importCells([cell], x, y, target);
@@ -276,14 +255,52 @@ window.onload = () => {
         }
     };
 
-    var dragEl = document.createElement('div');
-    dragEl.style.border = 'dashed black 1px';
-    dragEl.style.width = '140px';
-    dragEl.style.height = '80px';
-    dragEl.style.borderRadius = '10px';
+    var dragEl1 = document.createElement('div');
+    dragEl1.style.border = 'dashed grey 1px';
+    dragEl1.style.width = '140px';
+    dragEl1.style.height = '80px';
+    dragEl1.style.borderRadius = '10px';
 
-    var dragSource = mxUtils.makeDraggable(document.getElementById('newNode'), () => graph, afterDrag, dragEl, null, null, graph.autoscroll, true);
-    dragSource.isGuidesEnabled = function () { return graph.graphHandler.guidesEnabled; };
+    var dragSource1 = mxUtils.makeDraggable(q('#newNode'), () => graph, afterDrag1, dragEl1, null, null, graph.autoscroll, true);
+    dragSource1.isGuidesEnabled = function () { return graph.graphHandler.guidesEnabled; };
+
+    var afterDrag2 = function (graph, evt, target, x, y) {
+        var cell = new mxCell(null, new mxGeometry(0, 0, 50, 50));
+        cell.vertex = true;
+        cell.style = startStyle;
+        var cells = graph.importCells([cell], x, y, target);
+        addOverlays(graph, cells[0]);
+
+        if (cells != null && cells.length > 0) {
+            graph.scrollCellToVisible(cells[0]);
+            graph.setSelectionCells(cells);
+        }
+    };
+
+    var afterDrag3 = function (graph, evt, target, x, y) {
+        var cell = new mxCell(null, new mxGeometry(0, 0, 50, 50));
+        cell.vertex = true;
+        cell.style = endStyle;
+        var cells = graph.importCells([cell], x, y, target);
+        addOverlays(graph, cells[0]);
+
+        if (cells != null && cells.length > 0) {
+            graph.scrollCellToVisible(cells[0]);
+            graph.setSelectionCells(cells);
+        }
+    };
+
+    var dragEl2 = document.createElement('div');
+    dragEl2.style.border = 'dashed grey 2px';
+    dragEl2.style.width = '50px';
+    dragEl2.style.height = '50px';
+    dragEl2.style.borderRadius = '50px';
+
+    var dragSource2 = mxUtils.makeDraggable(q('#newStart'), () => graph, afterDrag2, dragEl2, null, null, graph.autoscroll, true);
+    dragSource2.isGuidesEnabled = function () { return graph.graphHandler.guidesEnabled; };
+
+    var dragSource3 = mxUtils.makeDraggable(q('#newEnd'), () => graph, afterDrag3, dragEl2, null, null, graph.autoscroll, true);
+    dragSource3.isGuidesEnabled = function () { return graph.graphHandler.guidesEnabled; };
 
     function addOverlays(graph, cell) {
         if (cell.id == '_start') return;
@@ -332,10 +349,48 @@ window.onload = () => {
             return true;
         });
 
-        if (cells.length > 1) {
-            if (confirm('Deze stap en de onderliggende stappen worden verwijderd.\nWeet je het zeker?'))
-                graph.removeCells(cells);
-        }
-        else graph.removeCells(cells);
+        if (confirm('Dit item en eventueel onderliggende items worden verwijderd.\nWeet je het zeker?'))
+            graph.removeCells(cells);
     }
+
+    //Selection / change fields
+    graph.getSelectionModel().addListener(mxEvent.CHANGE, function (sender, evt) {
+        var cells = graph.getSelectionCells();
+        var cell = cells[0];
+
+        if (cells.length === 1 && cell && !(cell.style || '').includes('ellipse')) {
+            q('#itemTitle').style.display = '';
+            q('#itemDesc').style.display = '';
+            q('textarea', '#itemTitle').value = cell.value;
+        }
+        else {
+            q('#itemTitle').style.display = 'none';
+            q('#itemDesc').style.display = 'none';
+        }
+    });
+
+    q('textarea', '#itemTitle').onfocus = function(e) {
+        graph.stopEditing();
+    };
+
+    q('textarea', '#itemTitle').onkeyup = function (e) {
+        var newValue = graph.getSelectionCell().value = e.target.value;
+        graph.getModel().beginUpdate();
+
+        try {
+            var cell = graph.getSelectionCell();
+            var edit = new mxCellAttributeChange(cell, 'value', newValue);
+            graph.getModel().execute(edit);
+            //graph.updateCellSize(cell);
+        }
+        finally {
+            graph.getModel().endUpdate();
+        }
+    };
+
+    q('[contentEditable]', '#itemDesc').onkeyup = function(e) {
+        var cell = graph.getSelectionCell();
+        console.log(cell);
+        cell.setAttribute('test', '123');
+    };
 };
